e3d8e84ad63a634cdb97c634c0f2d5b5e410c839
ImaWijesekara <218763457+ImaWijesekara@users.noreply.github.com>
2026-08-18 23:50:53 +0530

    setup the backned using spring initializer

---
+++ jcb_mng_backend/src/main/java/com/se/jcb_mng/JcbMngApplication.java
@@ -0,0 +1,13 @@
+package com.se.jcb_mng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcbMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcbMngApplication.class, args);
+	}
+
+}
---
+++ jcb_mng_backend/src/test/java/com/se/jcb_mng/JcbMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.jcb_mng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcbMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
